7d8fd78077b82a451b1d2820e29798d90f154c9f
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2025-01-12 13:43:11 +0200

    prod

--- firebase.ts
+++ firebase.ts
@@ -6,14 +6,24 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBVKYJ3syMM2Li31jPbpNb13siMKNWptjI",
+//   authDomain: "shoppers-529c9.firebaseapp.com",
+//   projectId: "shoppers-529c9",
+//   storageBucket: "shoppers-529c9.firebasestorage.app",
+//   messagingSenderId: "963413293362",
+//   appId: "1:963413293362:web:6d0cf631f29c5ab6e8ff2b",
+//   measurementId: "G-VG7PLDXLC6",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBVKYJ3syMM2Li31jPbpNb13siMKNWptjI",
-  authDomain: "shoppers-529c9.firebaseapp.com",
-  projectId: "shoppers-529c9",
-  storageBucket: "shoppers-529c9.firebasestorage.app",
-  messagingSenderId: "963413293362",
-  appId: "1:963413293362:web:6d0cf631f29c5ab6e8ff2b",
-  measurementId: "G-VG7PLDXLC6",
+  apiKey: "AIzaSyAX9IDX09KoyNspos-jMIcVHjlQ1C18vlM",
+  authDomain: "shop-prod-a45b5.firebaseapp.com",
+  projectId: "shop-prod-a45b5",
+  storageBucket: "shop-prod-a45b5.firebasestorage.app",
+  messagingSenderId: "629330203584",
+  appId: "1:629330203584:web:6f38222a20669fd0dbee25"
 };
 
 // Initialize Firebase
